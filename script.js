068306a5c4f70e64522cbbcb52e9527a8fe33298
Bobby Hubbard <bghubbard392@gmail.com>
2021-12-17 00:41:19 -0600

    added game fu nction, and updated roShamBo funtion to playRound adding in a return value to increase player or computer score

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let playerScore = 0
+let computerScore = 0
+
+
+
 
 function computerPlay(a, b, c) { //Selects a random choice out of function parameters a, b, c
     var arr = [a, b, c]
@@ -5,18 +10,28 @@ function computerPlay(a, b, c) { //Selects a random choice out of function param
     return random
 }
 
-function roShamBo(a, b) {
+function playRound(a, b){
     let computerSelection = computerPlay('rock', 'paper', 'scissors')
-    let playerSelection = prompt('Enter rock, paper, or scissors').toLowerCase()
-        if ((computerSelection === 'rock') && (playerSelection === 'scissors')) {console.log('You Lose! Rock beats scissors.')}
-        else if ((computerSelection === 'rock') && (playerSelection === 'rock')) {console.log('It\'s a tie! You both chose rock!')}
-        else if ((computerSelection === 'rock') && (playerSelection === 'paper')) {console.log('You Win! Paper beats rock!')}
-        else if ((computerSelection === 'scissors') && (playerSelection === 'paper')) {console.log('You Lose! Scissors beats paper!')}
-        else if ((computerSelection === 'scissors') && (playerSelection === 'rock')) {console.log('You Win! Rock beats scissors!')}
-        else if ((computerSelection === 'scissors') && (playerSelection === 'scissors')) {console.log('It\'s a tie! You both chose scissors!')}
-        else if ((computerSelection === 'paper') && (playerSelection === 'rock')) {console.log('You Lose! Paper beats rock!')}
-        else if ((computerSelection === 'paper') && (playerSelection === 'scissors')) {console.log('You Win! Scissors beats paper!')}
-        else if ((computerSelection === 'paper') && (playerSelection === 'paper')) {console.log('It\'s a tie! You both chose paper!')}
-}
+    let playerSelection = prompt('Enter rock, paper, or scissors')
+    if ((computerSelection === 'rock') && (playerSelection === 'scissors')) {console.log('You Lose! Rock beats scissors.');return computerScore = (computerScore + 1)}
+    else if ((computerSelection === 'rock') && (playerSelection === 'rock')) {console.log('It\'s a tie! You both chose rock!')}
+    else if ((computerSelection === 'rock') && (playerSelection === 'paper')) {console.log('You Win! Paper beats rock!');return playerScore = (playerScore +1)}
+    else if ((computerSelection === 'scissors') && (playerSelection === 'paper')) {console.log('You Lose! Scissors beats paper!');return computerScore = (computerScore + 1)}
+    else if ((computerSelection === 'scissors') && (playerSelection === 'rock')) {console.log('You Win! Rock beats scissors!');return playerScore = (playerScore +1)}
+    else if ((computerSelection === 'scissors') && (playerSelection === 'scissors')) {console.log('It\'s a tie! You both chose scissors!')}
+    else if ((computerSelection === 'paper') && (playerSelection === 'rock')) {console.log('You Lose! Paper beats rock!');return computerScore = (computerScore + 1)}
+    else if ((computerSelection === 'paper') && (playerSelection === 'scissors')) {console.log('You Win! Scissors beats paper!');return playerScore = (playerScore +1)}
+    else if ((computerSelection === 'paper') && (playerSelection === 'paper')) {console.log('It\'s a tie! You both chose paper!')}
+    }
 
-roShamBo()
+
+
+function game(){
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+    console.log(`Computer score ${computerScore}, Player score ${playerScore}`)
+}
+game()
