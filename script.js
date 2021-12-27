1c9ba8db0fd3d27b2e8c89f27b4c85684972ac7f
Bobby Hubbard <bghubbard392@gmail.com>
2021-12-27 00:01:04 -0600

    commit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function computerPlay(a, b, c) { //Selects a random choice out of function param
     return random
 }
 
-function playRound(a, b){ //Prompts user for a choice and then compares it to the computer's random selection. Then updates the score variable depending on outcome.
+function playRound(){ //Prompts user for a choice and then compares it to the computer's random selection. Then updates the score variable depending on outcome.
     let computerSelection = computerPlay('rock', 'paper', 'scissors')
     let playerSelection = prompt('Enter rock, paper, or scissors').toLowerCase()
     if (computerSelection === playerSelection)
@@ -27,20 +27,6 @@ function playRound(a, b){ //Prompts user for a choice and then compares it to th
         return `You Win! ${playerSelection} beats ${computerSelection}`} //if the player wins, returns a string that says so and increments the player score.
 }
 
-
-
-
-
-    // if ((computerSelection === 'rock') && (playerSelection === 'scissors')) {console.log('You Lose! Rock beats scissors.');return computerScore = (computerScore + 1)}
-    // else if ((computerSelection === 'rock') && (playerSelection === 'rock')) {console.log('It\'s a tie! You both chose rock!')}
-    // else if ((computerSelection === 'rock') && (playerSelection === 'paper')) {console.log('You Win! Paper beats rock!');return playerScore = (playerScore +1)}
-    // else if ((computerSelection === 'scissors') && (playerSelection === 'paper')) {console.log('You Lose! Scissors beats paper!');return computerScore = (computerScore + 1)}
-    // else if ((computerSelection === 'scissors') && (playerSelection === 'rock')) {console.log('You Win! Rock beats scissors!');return playerScore = (playerScore +1)}
-    // else if ((computerSelection === 'scissors') && (playerSelection === 'scissors')) {console.log('It\'s a tie! You both chose scissors!')}
-    // else if ((computerSelection === 'paper') && (playerSelection === 'rock')) {console.log('You Lose! Paper beats rock!');return computerScore = (computerScore + 1)}
-    // else if ((computerSelection === 'paper') && (playerSelection === 'scissors')) {console.log('You Win! Scissors beats paper!');return playerScore = (playerScore +1)}
-    // else if ((computerSelection === 'paper') && (playerSelection === 'paper')) {console.log('It\'s a tie! You both chose paper!')}
-    // }
 
 
 
